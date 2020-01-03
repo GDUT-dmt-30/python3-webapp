b6f0119ce98771ba416680ed22a218b4d5b13bff
GDUT-gmt-30 <1535277375@qq.com>
2020-01-03 08:59:07 +0800

    test

---
+++ www/app.py
@@ -0,0 +1,15 @@
+import logging; logging.basicConfig(level = logging.INFO)
+
+import asyncio,os,json,time
+from datetime import datetime
+
+from aiohttp import web
+
+'''app注释'''
+
+def index(request):
+    return web.Response(body=b'<h1>Awesome</h1>')
+
+@asynvio.coroutine
+def init
+
